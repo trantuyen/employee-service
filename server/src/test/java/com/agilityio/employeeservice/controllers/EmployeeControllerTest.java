d1fa26611302d3c3abc851cde52f73de66abe1a1
Tuyen Tran <tuyen.tran@asnet.com.vn>
2018-06-06 09:35:05 +0700

    Add todos for implementation unit tests

--- server/src/test/java/com/agilityio/employeeservice/controllers/EmployeeControllerTest.java
+++ server/src/test/java/com/agilityio/employeeservice/controllers/EmployeeControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Locale;
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -49,16 +51,51 @@ public class EmployeeControllerTest {
         repository.deleteAll();
     }
 
+    /**
+     * Test finding employees of a department success.
+     *
+     * @throws Exception Json processing error
+     */
     @Test
     public void testFindOneSuccess() throws Exception {
-        // Perform get all departments
+        // TODO:: Create department first.
+        // TODO:: Create employees of the department
+
+        // Perform get all employees
+        // TODO:: Get by department
         mockMvc.perform(get(baseUrlTemplate))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$").isNotEmpty())
             .andExpect(jsonPath("$").isArray());
     }
 
+    /**
+     * Test finding employees of a department got not found status.
+     *
+     * @throws Exception Json processing error
+     */
     @Test
-    public void createOrUpdate() {
+    public void testFindOneWithInvalidDepartmentId() throws Exception {
+        // Perform get all departments
+        mockMvc.perform(get(baseUrlTemplate + "?departmentId=" + UUID.randomUUID().toString()))
+            .andExpect(jsonPath("$.code").value(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * Test create employees of a department success
+     */
+    @Test
+    public void testCreateSuccess() {
+        // TODO:: Create department first.
+        // TODO:: Create employees of the department
+    }
+
+    /**
+     * Test create employees of a department success
+     */
+    @Test
+    public void testCreateSuccess() {
+        // TODO:: Create department first.
+        // TODO:: Create employees of the department
     }
 }
